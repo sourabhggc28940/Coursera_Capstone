8d781a7d7ec42101144d55dcf27a64f3cb02a5f7
sourabhggc28940 <53007652+sourabhggc28940@users.noreply.github.com>
2019-07-17 19:50:47 +0530

    Add files via upload

---
+++ CAPSTONE 1.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[4]:
+
+
+df =  pd.read_csv('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M')
+
+
+# In[5]:
+
+
+df1 = pd.read_csv('https://cocl.us/Geospatial_data')
+
+
+# In[6]:
+
+
+df1
+
